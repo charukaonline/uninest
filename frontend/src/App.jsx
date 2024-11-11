836674b94423d0c346d077af97a36a167c412682
charukaonline <nuwanthakonline@gmail.com>
2024-11-11 19:26:28 +0530

    #

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from "react-router-dom";
+
 import UserSignupPage from "./pages/UserSignupPage";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -7,7 +9,7 @@ function App() {
       <Routes>
 
         {/* Main pages */}
-        <Route path="/" element={"Landing page"} />
+        <Route path="/" element={<Home />} />
 
         {/* Student signup and signin */}
         <Route path="/auth/user-signup" element={<UserSignupPage />} />
