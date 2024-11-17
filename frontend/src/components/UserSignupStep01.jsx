3f49c08af6bad9016834ad28418742c1c4f711b8
charukaonline <nuwanthakonline@gmail.com>
2024-11-17 12:30:58 +0530

    student signup 1 fixes

--- frontend/src/components/UserSignupStep01.jsx
+++ frontend/src/components/UserSignupStep01.jsx
@@ -1,40 +1,41 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState } from "react";
 import { Form, Input, Divider } from "antd";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { FcGoogle } from "react-icons/fc";
 import { Button } from "./ui/button";
 
 const UserSignupStep01 = ({ onFinish }) => {
   const [form] = Form.useForm();
+  const [isGoogleHovered, setIsGoogleHovered] = useState(false);
 
   const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
-  };
-
-  const googleButtonStyle = {
-    backgroundColor: isGoogleHovered ? "#15803d" : "#006845",
-    color: "white",
-    borderRadius: "999px",
-    border: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: "8px",
-    width: "100%",
-    cursor: "pointer",
+    console.log("Form submission failed:", errorInfo);
   };
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100 p-4">
       <div className="w-full max-w-md p-6 sm:p-8 bg-white shadow-lg rounded-md">
         <div className="text-right text-xs">
-          Already have an account? <a href="#" className="underline">Login Now</a>
+          Already have an account?{" "}
+          <a href="#" className="underline">
+            Login Now
+          </a>
         </div>
         <br />
 
         <button
-          style={googleButtonStyle}
+          style={{
+            backgroundColor: isGoogleHovered ? "#15803d" : "#006845",
+            color: "white",
+            borderRadius: "999px",
+            border: "none",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "8px",
+            width: "100%",
+            cursor: "pointer",
+          }}
           onMouseEnter={() => setIsGoogleHovered(true)}
           onMouseLeave={() => setIsGoogleHovered(false)}
         >
@@ -42,7 +43,12 @@ const UserSignupStep01 = ({ onFinish }) => {
           <div className="font-bold text-base">Continue with Google</div>
         </button>
 
-        <Divider className="my-6 sm:my-8" style={{ color: "black", borderColor: "black" }}>Or</Divider>
+        <Divider
+          className="my-6 sm:my-8"
+          style={{ color: "black", borderColor: "black" }}
+        >
+          Or
+        </Divider>
 
         <Form
           form={form}
@@ -52,24 +58,24 @@ const UserSignupStep01 = ({ onFinish }) => {
           onFinishFailed={onFinishFailed}
           layout="vertical"
         >
-          <h2 className="text-xl font-semibold mb-4 text-center">Create an Account</h2>
+          <h2 className="text-xl font-semibold mb-4 text-center">
+            Create an Account
+          </h2>
 
           <Form.Item
             label="Email Address"
             name="email"
             rules={[{ required: true, message: "Please input your email!" }]}
-            className={"font-medium"}
           >
-            <Input style={{ borderColor: "#006845", borderWidth: "1px", outline: "none", }} placeholder={"Email Addresss"} />
+            <Input placeholder="Email Address" />
           </Form.Item>
 
           <Form.Item
-            label="User name"
+            label="Username"
             name="username"
             rules={[{ required: true, message: "Please input your username!" }]}
-            className={"font-medium"}
           >
-            <Input style={{ borderColor: "#006845", borderWidth: "1px", outline: "none", }} placeholder={"Username"} />
+            <Input placeholder="Username" />
           </Form.Item>
 
           <Form.Item
@@ -77,12 +83,13 @@ const UserSignupStep01 = ({ onFinish }) => {
             name="password"
             rules={[
               { required: true, message: "Please input your password!" },
-              { min: 8, message: "Password must be at least 8 characters long!" },
+              {
+                min: 8,
+                message: "Password must be at least 8 characters long!",
+              },
             ]}
-            className={"font-medium"}
-            hasFeedback
           >
-            <Input.Password style={{ borderColor: "#006845", borderWidth: "1px", outline: "none", }} placeholder={"Password"} />
+            <Input.Password placeholder="Password" />
           </Form.Item>
 
           <Form.Item
@@ -100,56 +107,42 @@ const UserSignupStep01 = ({ onFinish }) => {
                 },
               }),
             ]}
-            className={"font-medium"}
-            hasFeedback
           >
-            <Input.Password style={{ borderColor: "#006845", borderWidth: "1px", outline: "none", }} placeholder={"Re-Enter Password"} />
+            <Input.Password placeholder="Re-enter Password" />
           </Form.Item>
 
           <Form.Item
-            name="agreement"
-            valuePropName="checked"
-            rules={[
-              {
-                validator: (_, value) =>
-                  value
-                    ? Promise.resolve()
-                    : Promise.reject(new Error("You must accept the terms and conditions!")),
-              },
-            ]}
-          >
-            <div className="mt-4 text-xs flex items-center">
-              <input type="checkbox" id="checkbox" className="accent-green-700 mr-2" />
-              <label htmlFor="checkbox">
-                By creating an account, I agree to our{" "}
-                <a href="#" className="underline">Terms of use</a> and <a href="#" className="underline">Privacy Policy</a>
-              </label>
-            </div>
-          </Form.Item>
+                name="agreement"
+                valuePropName="checked"
+                rules={[
+                  {
+                    validator: (_, value) =>
+                        value
+                            ? Promise.resolve()
+                            : Promise.reject(new Error("You must accept the terms and conditions!")),
+                  },
+                ]}
+            >
+              <div className="mt-4 text-xs flex items-center">
+                <input type="checkbox" id="checkbox" className="accent-green-700 mr-2" />
+                <label htmlFor="checkbox">
+                  By creating an account, I agree to our{" "}
+                  <a href="#" className="underline">Terms of use</a> and <a href="#" className="underline">Privacy Policy</a>
+                </label>
+              </div>
+            </Form.Item>
 
           <Form.Item>
-            <div
-              onMouseEnter={() => setIsSignUpHovered(true)}
-              onMouseLeave={() => setIsSignUpHovered(false)}
-            >
-              <Button
-                type="submit"
-                block
-                className="mb-4 bg-green-700 rounded-full"
-                disabled={
-                  !form.isFieldsTouched(true) ||
-                  form.getFieldsError().filter(({ errors }) => errors.length).length > 0
-                }
-              >
-                SignUp
-              </Button>
-            </div>
+            <Button type="submit" block className="bg-green-700 rounded-full w-full ">
+              Sign Up
+            </Button>
           </Form.Item>
         </Form>
       </div>
     </div>
   );
 };
+
 UserSignupStep01.propTypes = {
   onFinish: PropTypes.func.isRequired,
 };
