b9b5b7718c3e315df48838448c3d115e54ea4df2
charukaonline <nuwanthakonline@gmail.com>
2025-02-06 09:38:13 +0530

    file structure fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,117 @@
 import { Route, Routes } from "react-router-dom";
 
-import UserSignupPage from "./pages/UserSignupPage";
+import UserSignupPage from "./pages/(auth)/UserSignupPage";
 import Home from "./pages/Home";
 import Layout from "./components/Layout";
-import UserSigninPage from "./pages/UserSigninPage";
-import HouseownerSigninPage from "./pages/HouseownerSigninPage";
-import HouseownerSignupPage from "./pages/HouseownerSignupPage";
+import UserSigninPage from "./pages/(auth)/UserSigninPage";
+import HouseownerSigninPage from "./pages/(auth)/HouseownerSigninPage";
+import HouseownerSignupPage from "./pages/(auth)/HouseownerSignupPage";
+import PendingHouseowner from "./pages/(auth)/PendingHouseowner";
 import AllListings from "./pages/AllListings";
 import PropertyInfo from "./pages/PropertyInfo";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
-import StudentDashboard from "./pages/StdDashboard/StdDashboard";
-import AdminDashboard from "./pages/AdminDashboard/AdminDashboard";
+import StudentDashboard from "./pages/(StdDashboard)/StdDashboard";
+import AdminDashboard from "./pages/(AdminDashboard)/AdminDashboard";
 import NotFound from "./pages/404Page";
 import UserPreference from "./components/signup_pages/UserPreference";
 import ProtectedRoute from "./components/ProtectedRoute";
 import useAuthInitializer from "./store/authStore";
-import AdminLogin from "./pages/AdminLogin";
+import AdminLogin from "./pages/(auth)/AdminLogin";
 
 function App() {
-
   useAuthInitializer();
 
   return (
     <>
       <Routes>
         {/* Main pages */}
-        <Route path="/" element={<Layout><Home /></Layout>} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
         <Route path="*" element={<NotFound />} />
 
         {/* Student signup and signin */}
-        <Route path="/auth/user-signup" element={<Layout><UserSignupPage /></Layout>} />
-        <Route path="/auth/user-signin" element={<Layout><UserSigninPage /></Layout>} />
-        <Route path="/auth/google/success" element={<Layout><UserPreference /></Layout>} />
+        <Route
+          path="/auth/user-signup"
+          element={
+            <Layout>
+              <UserSignupPage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/auth/user-signin"
+          element={
+            <Layout>
+              <UserSigninPage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/auth/google/success"
+          element={
+            <Layout>
+              <UserPreference />
+            </Layout>
+          }
+        />
 
         {/* House owners signup and signin */}
-        <Route path='/auth/houseowner-signup' element={<Layout><HouseownerSignupPage /></Layout>} />
-        <Route path='/auth/houseowner-signin' element={<Layout><HouseownerSigninPage /></Layout>} />
+        <Route
+          path="/auth/houseowner-signup"
+          element={
+            <Layout>
+              <HouseownerSignupPage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/auth/verification-pending"
+          element={<PendingHouseowner />}
+        />
+        <Route
+          path="/auth/houseowner-signin"
+          element={
+            <Layout>
+              <HouseownerSigninPage />
+            </Layout>
+          }
+        />
 
         {/* All Listings */}
-        <Route path='/all-listings' element={<Layout><AllListings /></Layout>} />
+        <Route
+          path="/all-listings"
+          element={
+            <Layout>
+              <AllListings />
+            </Layout>
+          }
+        />
 
         {/* One Property Details */}
-        <Route path='/property/:propertyId' element={<Layout><PropertyInfo /></Layout>} />
+        <Route
+          path="/property/:propertyId"
+          element={
+            <Layout>
+              <PropertyInfo />
+            </Layout>
+          }
+        />
 
         {/* Privacy Policy */}
-        <Route path='/privacy-policy' element={<Layout><PrivacyPolicy /></Layout>} />
+        <Route
+          path="/privacy-policy"
+          element={
+            <Layout>
+              <PrivacyPolicy />
+            </Layout>
+          }
+        />
 
         {/* Student Dashboard */}
         <Route element={<ProtectedRoute />}>
@@ -55,7 +122,7 @@ function App() {
         <Route path="/auth/uninest-admin" element={<AdminLogin />} />
 
         {/* Admin Dashboard */}
-        <Route path='/admin-dashboard' element={<AdminDashboard />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
       </Routes>
     </>
   );
--- frontend/src/components/signup_pages/HouseownerSignup1.jsx
+++ frontend/src/components/signup_pages/HouseownerSignup1.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
-import { Form, Input, Divider } from "antd";
+import { Form, Input } from "antd";
 
 const HouseownerSignup1 = ({ onFinish, loading }) => {
 
